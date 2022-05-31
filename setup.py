4a1e1fd87f0e12a1a54224e868b2fb0fdbae9556
ruben970105 <Bucanero06$1>
2022-05-31 19:39:54 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-import os
-import sys
-
 from setuptools import setup
 
 setup(
     name='mini_Genie',
-    version='0.1',
+    version='0.1.0',
     packages=['Utilities', 'Data_Handler', 'Error_Handler', 'Analysis_Handler', 'Equipment_Handler',
               'mini_Genie_Object', 'Simulation_Handler', 'Optimization_Module_mini_genie'],
     package_dir={'': 'mini_genie_source'},
     url='',
     license='',
-    author='ruben',
+    author='Ruben Fernandez Carbon',
     author_email='',
-    description='An efficient ab-initio method of simulating the financial markets on billions of parameter combinations'
+    description='An efficient ab-initio method of simulating the financial markets on billions of parameter combinations  '
 )
-
-os.system("hiiijoii")
