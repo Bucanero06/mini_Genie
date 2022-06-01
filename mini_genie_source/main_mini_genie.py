00e472021a062ba8fc8dda9c5a8c1636a21181ce
ruben970105 <Bucanero06$1>
2022-06-01 04:06:36 -0400

    -

--- mini_genie_source/main_mini_genie.py
+++ mini_genie_source/main_mini_genie.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Help for mini-Genie Trader")
     #
     parser.add_argument("-gp", help="Simulate using genie picked space based on user settings", dest="genie_pick",
-                        action='store_true', default=False)
+                        action='store_true', default=True)
     parser.add_argument("-up", help="Simulate using solely the user picked space", dest="user_pick",
                         action='store_true', default=False)
     parser.add_argument("-tsv",
